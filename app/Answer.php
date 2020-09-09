b1cca63f22c2dd3c6501e655fe63412bc924360e
kevin ard <muhammad.ardela@dana.id>
2020-09-09 13:31:51 +0700

    Answer seeder

--- app/Answer.php
+++ app/Answer.php
@@ -20,4 +20,14 @@ class Answer extends Model
         # code...
         return $this->belongsTo(User::class);
     }
+
+    public static function boot()
+    {
+        # code...
+        parent::boot();
+
+        static::created(function($answer){
+            $answer->question->increment('answers_count');
+        });
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\Answer;
 use App\Question;
 use Illuminate\Database\Seeder;
 
@@ -16,12 +17,17 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         // factory(User::class, 3)->create();
         // factory(Question::class, 20)->create();
-        factory(User::class, 3)->create()->each(function ($u)
-        {
-            # code...
+        factory(User::class, 3)->create()
+        ->each(function ($u){
             $u->questions()
               ->saveMany(
-                factory(Question::class, rand(1,5))->make());
+                factory(Question::class, rand(1,5))->make())
+                ->each(function($q){
+                    $q->answers()
+                      ->saveMany(
+                          factory(Answer::class, rand(1,5))->make()
+                      );
+                });
         });
     }
 }
